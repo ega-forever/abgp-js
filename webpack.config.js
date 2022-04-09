415c5dc3875efa8f25f504c6a8febb953f712520
zuevegor <zyev.egor@gmail.com>
2022-04-09 22:14:35 +0300

    *model optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const webpack = require('webpack');
 
 module.exports = {
+  mode: 'production',
   entry: './src/consensus/main.ts',
   module: {
     rules: [
@@ -16,15 +18,30 @@ module.exports = {
     new BundleAnalyzerPlugin({
       analyzerMode: 'static',
       reportFilename: path.join(__dirname, 'webpack_report.html')
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer']
     })
   ],
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
+    fallback: {
+      fs: false,
+      tls: false,
+      net: false,
+      path: false,
+      zlib: false,
+      http: false,
+      https: false,
+      stream: false,
+      buffer: require.resolve('buffer'),
+      crypto: require.resolve('crypto-browserify') // if you want to use this module also don't forget npm i crypto-browserify
+    }
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist', 'web'),
-    library: 'BGP',
+    library: 'ABGP',
     libraryTarget: 'umd'
   }
 };
